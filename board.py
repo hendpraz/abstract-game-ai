5edf1b4d174940ee3b849e9520ef434b0be352be
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-09-30 17:39:02 +0700

    Rename n to board_size

--- board.py
+++ board.py
@@ -9,12 +9,14 @@ dir_y = [-1, -1, -1, 0, 0, 1, 1, 1]
 
 
 def init_board():
-    if board_size % 2 == 0:
-        z = int((board_size - 2) / 2)
-        board[z][z] = '2'
-        board[board_size - 1 - z][z] = '1'
-        board[z][board_size - 1 - z] = '1'
-        board[board_size - 1 - z][board_size - 1 - z] = '2'
+    # c == center
+    c_low = int((board_size - 2) / 2)
+    c_high = board_size - 1 - c_low
+
+    board[c_low][c_low] = '2'
+    board[c_high][c_low] = '1'
+    board[c_low][c_high] = '1'
+    board[c_high][c_high] = '2'
 
 
 def print_board():
@@ -33,54 +35,64 @@ def print_board():
 
 
 def execute_move(current_board, x, y, player):  # assuming valid move
+
     num_opponent_piece_taken = 0
     current_board[y][x] = player
+
     for d in range(8):  # 8 directions
-        ctr = 0
+        piece_taken = 0
+
         for i in range(board_size):
             dx = x + dir_x[d] * (i + 1)
             dy = y + dir_y[d] * (i + 1)
-            if dx < 0 or dx > board_size - 1 or dy < 0 or dy > board_size - 1:
-                ctr = 0
+
+            if not(0 < dx < board_size and 0 < dy < board_size):
+                piece_taken = 0
                 break
             elif current_board[dy][dx] == player:
                 break
             elif current_board[dy][dx] == '0':
-                ctr = 0
+                piece_taken = 0
                 break
             else:
-                ctr += 1
-        for i in range(ctr):
+                piece_taken += 1
+
+        for i in range(piece_taken):
             dx = x + dir_x[d] * (i + 1)
             dy = y + dir_y[d] * (i + 1)
             current_board[dy][dx] = player
-        num_opponent_piece_taken += ctr
+
+        num_opponent_piece_taken += piece_taken
+
     return current_board, num_opponent_piece_taken
 
 
 def is_legal_move(current_board, x, y, player):
-    if x < 0 or x > board_size - 1 or y < 0 or y > board_size - 1:
+    if not(0 < x < board_size and 0 < y < board_size):
         return False
+
     if current_board[y][x] != '0':
         return False
-    (boardTemp, tot_ctr) = execute_move(copy.deepcopy(current_board), x, y, player)
-    if tot_ctr == 0:
+
+    board_temp, total_piece_taken = execute_move(copy.deepcopy(current_board), x, y, player)
+    if total_piece_taken == 0:
         return False
+
     return True
 
 
 def eval_board(current_board, current_player):
-    tot = 0
+    score = 0
     for y in range(board_size):
         for x in range(board_size):
             if current_board[y][x] == current_player:
                 if (x == 0 or x == board_size - 1) and (y == 0 or y == board_size - 1):
-                    tot += 4  # corner
+                    score += 4  # corner
                 elif (x == 0 or x == board_size - 1) or (y == 0 or y == board_size - 1):
-                    tot += 2  # side
+                    score += 2  # side
                 else:
-                    tot += 1
-    return tot
+                    score += 1
+    return score
 
 
 def is_terminal_node(current_board, current_player):
@@ -89,14 +101,3 @@ def is_terminal_node(current_board, current_player):
             if is_legal_move(current_board, x, y, current_player):
                 return False
     return True
-
-
-class Board:
-
-    def __init__(self):
-        self.__board_size = 8
-        self.__board = [['0' for x in range(board_size)] for y in range(board_size)]
-        self.__dir_x = [-1, 0, 1, -1, 1, -1, 0, 1]
-        self.__dir_y = [-1, -1, -1, 0, 0, 1, 1, 1]
-
-
