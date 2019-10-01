39cd715b787947a06e352ceedc1f0603ef4229b3
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-10-01 22:39:47 +0700

    Move get_list_legal_move to board_checking.py

--- bot_minimax.py
+++ bot_minimax.py
@@ -1,7 +1,6 @@
 import copy
 
-from board_checking import is_terminal_node, is_legal_move
-from board_identity import board_size
+from board_checking import is_terminal_node, get_list_legal_moves
 from board_operation import eval_board, execute_move, min_eval_board, max_eval_board
 
 
@@ -9,25 +8,20 @@ def minimax(current_board, current_player, current_depth, maximizing_player):
     if current_depth == 0 or is_terminal_node(current_board, current_player):
         return eval_board(current_board, current_player)
 
+    legal_moves = get_list_legal_moves(current_board, current_player)
+
     if maximizing_player:
         best_value = min_eval_board
-
-        for y in range(board_size):
-            for x in range(board_size):
-                legal, message = is_legal_move(current_board, x, y, current_player)
-                if legal:
-                    board_temp, tot_ctr = execute_move(copy.deepcopy(current_board), x, y, current_player)
-                    v = minimax(board_temp, current_player, current_depth - 1, False)
-                    best_value = max(best_value, v)
+        for x, y in legal_moves:
+            board_temp, _ = execute_move(copy.deepcopy(current_board), x, y, current_player)
+            v = minimax(board_temp, current_player, current_depth - 1, False)
+            best_value = max(best_value, v)
 
     else:  # minimizingPlayer
         best_value = max_eval_board
-        for y in range(board_size):
-            for x in range(board_size):
-                legal, message = is_legal_move(current_board, x, y, current_player)
-                if legal:
-                    board_temp, tot_ctr = execute_move(copy.deepcopy(current_board), x, y, current_player)
-                    v = minimax(board_temp, current_player, current_depth - 1, True)
-                    best_value = min(best_value, v)
+        for x, y in legal_moves:
+            board_temp, _ = execute_move(copy.deepcopy(current_board), x, y, current_player)
+            v = minimax(board_temp, current_player, current_depth - 1, True)
+            best_value = min(best_value, v)
 
     return best_value
