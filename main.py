8609e485ee8edd8d7686f970ecab719438119771
Muhamad Nobel Fauzan <13517042@std.stei.itb.ac.id>
2019-10-01 07:14:14 +0700

    update GUI, board added

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import os
+import tkinter
+import time
 
 from engine import best_move
-from board import init_board, print_board, execute_move, is_legal_move, board, eval_board, is_terminal_node
+from board import init_board, print_board, execute_move, is_legal_move, board, eval_board, is_terminal_node, board_size
 
+m = tkinter.Tk()
+m.title("Othello AI")
+frame = tkinter.Frame(m)
+frame.pack()
+X = tkinter.IntVar()
+Y = tkinter.IntVar()
+X.set(-1)
+Y.set(-1)
+
+def setXY(r,c):
+    global X
+    global Y
+    X.set(r)
+    Y.set(c)
+
+#buttons = [[int(0) in range(board_size)] in range(board_size)]
+buttons = []
+for i in range(board_size):
+    temp = []
+    for j in range(board_size):
+        temp.append(tkinter.Button(frame,width=3,command=lambda k=i,l=j: setXY(l,k)))
+        temp[j].grid(row = i, column = j)
+    buttons.append(temp)
+
+def render(board):
+    global buttons
+    for i in range(board_size):
+        for j in range(board_size):
+            if board[i][j] != '0':
+                buttons[i][j]["text"] = board[i][j]
+
+buttons[3][3]["text"] = '2'
+buttons[4][4]["text"] = '2'
+buttons[4][3]["text"] = '1'
+buttons[3][4]["text"] = '1'
 
 if __name__ == '__main__':
 
@@ -40,19 +77,30 @@ if __name__ == '__main__':
 
             if player == '1':  # user's turn
                 while True:
-                    x_move_to, y_move_to = map(int, input('X Y: ').split())
+                    X.set(-1)
+                    frame.wait_variable(X)
+                    x_move_to = X.get()
+                    y_move_to = Y.get()
+                    buttons[y_move_to][x_move_to]["text"] = '1'
+                    # x_move_to, y_move_to = map(int, input('X Y: ').split())
 
                     if is_legal_move(board, x_move_to, y_move_to, player):
                         board, total_piece_taken = execute_move(board, x_move_to, y_move_to, player)
+                        render(board)
                         print('# of pieces taken: ' + str(total_piece_taken))
+                        m.update()
                         break
                     else:
                         print('Invalid move! Try again!')
 
             else:  # AI's turn
                 x_move_to, y_move_to = best_move(board, player, cpu_mode, deepest_depth)
-
+                buttons[y_move_to][x_move_to]["text"] = '2'
                 if (x_move_to, y_move_to) != (-1, -1):
                     board, total_piece_taken = execute_move(board, x_move_to, y_move_to, player)
+                    render(board)
+                    m.update()
                     print('AI played (X Y): ' + str(x_move_to) + ' ' + str(y_move_to))
                     print('# of pieces taken: ' + str(total_piece_taken))
+
+m.mainloop()
