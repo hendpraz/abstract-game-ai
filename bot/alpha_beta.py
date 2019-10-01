b4fa43e9ee30836250f4f0652ebee1fc914977a2
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-10-02 00:08:11 +0700

    Change to remaining_depth

--- bot/alpha_beta.py
+++ bot/alpha_beta.py
@@ -5,33 +5,35 @@ from board.identity import board_size
 from board.operation import eval_board, execute_move, min_eval_board, max_eval_board
 
 
-def alpha_beta(current_board, current_player, current_depth, alpha, beta, maximizing_player):
-    if current_depth == 0 or is_terminal_node(current_board, current_player):
+def alpha_beta(current_board, current_player, remaining_depth, alpha, beta, maximizing_player):
+    if remaining_depth == 0 or is_terminal_node(current_board, current_player):
         return eval_board(current_board, current_player)
 
     if maximizing_player:
         v = min_eval_board
         for y in range(board_size):
             for x in range(board_size):
-                legal, message = is_legal_move(current_board, x, y, current_player)
+                legal, _ = is_legal_move(current_board, x, y, current_player)
                 if legal:
                     board_temp, total_piece_taken = execute_move(copy.deepcopy(current_board), x, y, current_player)
-                    v = max(v, alpha_beta(board_temp, current_player, current_depth - 1, alpha, beta, False))
+                    v = max(v, alpha_beta(board_temp, current_player, remaining_depth - 1, alpha, beta, False))
+
                     alpha = max(alpha, v)
                     if beta <= alpha:
-                        break  # beta cut-off
+                        break
         return v
 
     else:  # minimizingPlayer
         v = max_eval_board
         for y in range(board_size):
             for x in range(board_size):
-                legal, message = is_legal_move(current_board, x, y, current_player)
+                legal, _ = is_legal_move(current_board, x, y, current_player)
                 if legal:
                     board_temp, total_piece_taken = execute_move(copy.deepcopy(current_board), x, y, current_player)
-                    v = min(v, alpha_beta(board_temp, current_player, current_depth - 1, alpha, beta, True))
+                    v = min(v, alpha_beta(board_temp, current_player, remaining_depth - 1, alpha, beta, True))
+
                     beta = min(beta, v)
                     if beta <= alpha:
-                        break  # alpha cut-off
+                        break
 
         return v
